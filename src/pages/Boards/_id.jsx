48d6751e93f5203e3ae69a5cc4c8ebf5667b6b76
Hiep <mrrdavidd@gmail.com>
2024-10-09 09:56:34 +0700

    add auth

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -2,7 +2,6 @@ import Container from '@mui/material/Container'
 import AppBar from '../../components/AppBar'
 import BoardBars from './BoardBars'
 import BoardContent from './BoardContent'
-import { Box } from '@mui/material'
 function Board() {
     return (
       <Container disableGutters maxWidth={false} sx={{
